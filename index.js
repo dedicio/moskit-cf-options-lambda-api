a08546356595abb26d6d7dd122479d2025e75b86
Dedicio Coelho <dedicio@gmail.com>
2022-02-18 14:15:55 -0300

    feat: add description

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/:customFieldId', get)
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    error: "Nenhuma informação encontrada! Para usar, passe o ID do campo personalizado na URL, exemplo: https://q39y788rasd.execute-api.us-east-1.amazonaws.com/CF_RerdAEwdrWdEr",
   })
 })
 
